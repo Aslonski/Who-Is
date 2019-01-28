bd6c1f6b7a793aa56459dcd8b462362c52ac87b3
Aslonski <andrey@intercom.io>
2019-01-28 13:43:38 -0600

    readme

--- app.rb
+++ app.rb
@@ -12,9 +12,43 @@ end
 
 post '/slack' do 
   puts "••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••"
- 
-   request_data = JSON.parse(request.body.read)
-   p request_data['type']
+ case request_data['type']
+       # When you enter your Events webhook URL into your app's Event Subscription settings, Slack verifies the
+       # URL's authenticity by sending a challenge token to your endpoint, expecting your app to echo it back.
+       # More info: https://api.slack.com/events/url_verification
+       when 'url_verification'
+         request_data['challenge']
+
+       when 'event_callback'
+         # Get the Team ID and Event data from the request object
+         team_id = request_data['team_id']
+         event_data = request_data['event']
+
+         # Events have a "type" attribute included in their payload, allowing you to handle different
+         # Event payloads as needed.
+         case event_data['type']
+           when 'team_join'
+             # Event handler for when a user joins a team
+             Events.user_join(team_id, event_data)
+           when 'reaction_added'
+             # Event handler for when a user reacts to a message or item
+             Events.reaction_added(team_id, event_data)
+           when 'pin_added'
+             # Event handler for when a user pins a message
+             Events.pin_added(team_id, event_data)
+           when 'message'
+             # Event handler for messages, including Share Message actions
+             Events.message(team_id, event_data)
+           else
+             # In the event we receive an event we didn't expect, we'll log it and move on.
+             puts "Unexpected event:\n"
+             puts JSON.pretty_generate(request_data)
+         end
+         # Return HTTP status code 200 so Slack knows we've received the Event
+         status 200
+     end
+   # request_data = JSON.parse(request.body.read)
+   # p request_data['type']
     # case request_data['type']
     # when 'url_verification'
       # When we receive a `url_verification` event, we need to
