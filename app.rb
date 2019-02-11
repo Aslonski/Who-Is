3bce277d7cbd832c79549a1b9af619f315ce98b1
Aslonski <andrey@intercom.io>
2019-02-11 10:55:17 -0600

    add logic for user not found

--- app.rb
+++ app.rb
@@ -30,7 +30,7 @@ post '/slack' do
    # end
   status 200
   get_real_user(extract_slack_ids)
-  p extract_slack_ids
+  extract_slack_ids
 end
 
 def get_real_user(*array_of_ids)
@@ -58,6 +58,7 @@ end
 def extract_slack_ids
    regex = $channel_topic.match(%r{CSE on call: <@(\w+).+<@(\w+)}m)
    return regex.captures
+   p regex.captures
 end
 
 #  case request_data['type']
