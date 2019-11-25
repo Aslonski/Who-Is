1187f6ff95a619a47c66d95f3c4a515163913596
Aslonski <andrey@intercom.io>
2019-11-25 11:16:50 -0600

    testing hash access

--- app.rb
+++ app.rb
@@ -28,7 +28,8 @@ end
 
 def set_on_call_people_in_intercom(people)
   hash_of_names = find_people_in_intercom(extract_names_from_topic(people))
-  p @cse_name
+  p @cse_name[0]
+  p hash_of_names
   p hash_of_names[@cse_name[0]]
 end
 
